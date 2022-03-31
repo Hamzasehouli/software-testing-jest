f3c37ac27bac82630cc65999b498c2aef6614811
Hamzasehouli <sehoulihamza@yahoo.com>
2022-03-31 20:27:14 +0000

    Added no to be equql mqtcher to make sure the clone function make an indpendant copy of an array

--- duplicateArray.js
+++ duplicateArray.js
@@ -2,4 +2,8 @@ const duplicateArray = function (arr) {
   return [...arr];
 };
 
+let a = 10;
+let b = a;
+a = 20;
+
 module.exports = duplicateArray;
--- duplicateArray.test.js
+++ duplicateArray.test.js
@@ -2,4 +2,5 @@ const duplicateArray = require("./duplicateArray");
 
 test("test array cloner function", () => {
   expect(duplicateArray([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  expect(duplicateArray([1, 2, 3, 4])).not.toBe([1, 2, 3, 4]);
 });
